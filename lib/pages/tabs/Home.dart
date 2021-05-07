a8c283c6462239fc45490cb56dc559b88373d8c8
熊壮 <xiongzhuang@e-elead.cn>
2021-05-07 18:20:12 +0800

    df

--- lib/pages/tabs/Home.dart
+++ lib/pages/tabs/Home.dart
@@ -1,7 +1,9 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_jd/config/Config.dart';
 import 'package:flutter_jd/model/FocusModel.dart';
+import 'package:flutter_jd/model/ProductModel.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 
@@ -13,20 +15,44 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
 
   List<FocusItemModel> _focusData  = [];
+
+  List<ProductItemModel> _hotProductData = [];
+
+  List<ProductItemModel> _recProductData = [];
   @override
   void initState(){
    super.initState();
    _getFocusData();
-   print(_focusData);
+   _getHotProduct();
   }
 
   //获取轮播图图片
   _getFocusData() async{
-     var api = "https://jd.itying.com/api/focus";
-     Map<String,dynamic> result = (await Dio().get(api)) as Map<String, dynamic>;
+     var api = "${Config.domain}api/focus";
+     var result = await Dio().get(api);
      setState(() {
-       _focusData =  FocusModel.fromJson(result).result;
+       _focusData =  FocusModel.fromJson(result.data).result;
      });
+  }
+
+  //获取猜你喜欢商品
+  _getHotProduct() async{
+    var api = "${Config.domain}api/plist?is_hot=1";
+    var result = await Dio().get(api);
+
+    setState(() {
+      _hotProductData = ProductModel.fromJson(result.data).result;
+    });
+  }
+
+  //获取热门推荐的商品
+  _getRecProduct() async{
+    var api = "${Config.domain}api/plist?is_best=1";
+    var result = await Dio().get(api);
+
+    setState(() {
+      _recProductData = ProductModel.fromJson(result.data).result;
+    });
   }
 
   //轮播图
@@ -37,8 +63,9 @@ class _HomePageState extends State<HomePage> {
           aspectRatio: 2 / 1,
           child: Swiper(
             itemBuilder: (BuildContext context, int index) {
-              return Image.network(
-                _focusData[index].url,
+              String pic=this._focusData[index].pic;
+              return new Image.network(
+                "${Config.domain}${pic.replaceAll('\\', '/')}",
                 fit: BoxFit.fill,
               );
             },
@@ -49,7 +76,9 @@ class _HomePageState extends State<HomePage> {
         ),
       );
     }else{
-      return Text("加载中");
+      return Container(
+        child: Text("加载中"),
+      );
     }
   }
 
@@ -68,7 +97,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  //热门商品
+  //猜你喜欢
   Widget _hotProduct() {
     return Container(
       height: 125.h,
@@ -77,19 +106,20 @@ class _HomePageState extends State<HomePage> {
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
+          String pic=this._hotProductData[index].pic;
           return Column(
             children: [
               Container(
                 height: 110.h,
                 margin: EdgeInsets.only(left: 10.w),
                 child: Image.network(
-                    "https://www.itying.com/images/flutter/hot${index + 1}.jpg",
+                    "${Config.domain}${pic.replaceAll('\\', '/')}",
                     fit: BoxFit.cover),
               )
             ],
           );
         },
-        itemCount: 10,
+        itemCount: _hotProductData.length,
       ),
     );
   }
@@ -102,69 +132,73 @@ class _HomePageState extends State<HomePage> {
         runSpacing: 10.h,
         spacing: 10.w,
         children: [
-          _recProductList(),
-          _recProductList(),
-          _recProductList(),
-          _recProductList(),
-          _recProductList(),
-          _recProductList()
+          // _recProductList(),
+          // _recProductList(),
+          // _recProductList(),
+          // _recProductList(),
+          // _recProductList(),
+          // _recProductList()
         ],
       ),
     );
   }
 
   //推荐商品
-  Widget _recProductList() {
-    var itemWidth = (ScreenUtil().screenWidth - 84) / 2;
+  List<Widget> _recProductList() {
+    List<Widget> productList = List();
 
-    return Container(
-      padding: EdgeInsets.all(5),
-      width: itemWidth.h,
-      decoration: BoxDecoration(
-          border:
-              Border.all(color: Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
-      child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            child: Image.network(
-              "https://www.itying.com/images/flutter/list1.jpg",
-              fit: BoxFit.cover,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.h),
-            child: Text(
-              "YIMU日本醋酸早春白色西装外套女春秋设计感小众2021新款高端套装",
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(color: Colors.black54),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 10.h),
-            child: Stack(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "￥188.0",
-                    style: TextStyle(color: Colors.red, fontSize: 16),
-                  ),
-                ),
-                Align(
-                  alignment: Alignment.centerRight,
-                  child: Text(
-                    "￥198.0",
-                    style: TextStyle(color: Colors.black54, fontSize: 14,decoration: TextDecoration.lineThrough),
-                  ),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+    // _recProductData.forEach((element) {
+    //
+    // });
+    // var itemWidth = (ScreenUtil().screenWidth - 35) / 2;
+    // return Container(
+    //   padding: EdgeInsets.all(5),
+    //   width: itemWidth,
+    //   decoration: BoxDecoration(
+    //       border:
+    //           Border.all(color: Color.fromRGBO(233, 233, 233, 0.9), width: 1)),
+    //   child: Column(
+    //     children: [
+    //       Container(
+    //         width: double.infinity,
+    //         child: Image.network(
+    //           "https://www.itying.com/images/flutter/list1.jpg",
+    //           fit: BoxFit.cover,
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: EdgeInsets.only(top: 10.h),
+    //         child: Text(
+    //           "YIMU日本醋酸早春白色西装外套女春秋设计感小众2021新款高端套装",
+    //           maxLines: 2,
+    //           overflow: TextOverflow.ellipsis,
+    //           style: TextStyle(color: Colors.black54),
+    //         ),
+    //       ),
+    //       Padding(
+    //         padding: EdgeInsets.only(top: 10.h),
+    //         child: Stack(
+    //           children: [
+    //             Align(
+    //               alignment: Alignment.centerLeft,
+    //               child: Text(
+    //                 "￥188.0",
+    //                 style: TextStyle(color: Colors.red, fontSize: 16),
+    //               ),
+    //             ),
+    //             Align(
+    //               alignment: Alignment.centerRight,
+    //               child: Text(
+    //                 "￥198.0",
+    //                 style: TextStyle(color: Colors.black54, fontSize: 14,decoration: TextDecoration.lineThrough),
+    //               ),
+    //             )
+    //           ],
+    //         ),
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 
   @override
